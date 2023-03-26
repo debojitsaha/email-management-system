1e60e1e281ff4a61805db2bb31cf045480ea5a63
Debojit Saha <saha05debojit@gmail.com>
2023-03-26 20:36:35 +0530

    create user API done. OTP todo

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,12 +2,55 @@ import { Request, Response } from "express";
 import { UserDto } from "../dtos/user.dto";
 import { createUser } from "../services/user.service";
 import { GenerateResponse } from "../utils/response.creator";
+import User from "../models/user.model";
+import bcrypt from "bcryptjs";
+var uniqueSlug = require("unique-slug");
 
+/**
+ * Controller to create a new user with unique email generation by the system after OTP verification.
+ *
+ * @param {Request} req express request interface
+ * @param {Response} res express response interface
+ * @returns {any} returns status code 201, the data recieved from the service & a message.
+ */
 const CreateUser = async (req: Request, res: Response): Promise<Response> => {
-  const user: UserDto = { ...req.body };
-  const { code, result = null, message } = await createUser(user);
+  try {
+    const user: UserDto = { ...req.body };
 
-  return GenerateResponse(res, code, result, message);
+    // Unique email generation
+    var randomSlug = uniqueSlug();
+    // console.log(randomSlug);
+
+    let email = user.name.split(" ").join(".") + randomSlug + "@mindwebs.com";
+
+    const fetchUserEmail = await User.findOne({ email: email });
+    if (fetchUserEmail) {
+      randomSlug = uniqueSlug("mindwebs@ems@3.0");
+      email = user.name.split(" ").join(".") + randomSlug + "@mindwebs.com";
+    }
+
+    email = email.toLowerCase();
+    user.email = email;
+
+    // Hashed password generation
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(user.password, salt);
+    user.password = secPass;
+
+    //DB Call to create new user.
+    const result = await createUser(user);
+
+    return GenerateResponse(res, 201, result, "OK");
+  } catch (err: any) {
+    if (String(process.env.DEBUG) === "TRUE") console.log(err);
+    if (
+      err.name === "ValidationError" ||
+      err.name == "CastError" ||
+      err.name == "BSONTypeError"
+    )
+      return GenerateResponse(res, 400, {}, "Wrong Input Format");
+    return GenerateResponse(res, 500);
+  }
 };
 
 export { CreateUser };
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,55 +1,16 @@
-import { UserDto } from "../dtos/user.dto";
+import { UserDto, UserSchemaDto } from "../dtos/user.dto";
 import User from "../models/user.model";
-var uniqueSlug = require("unique-slug");
-import bcrypt from "bcryptjs";
-const twilio = require("twilio");
 
-const createUser = async (user: UserDto) => {
-  try {
-    const accountSid = process.env.TWILIO_ACCOUNT_SID;
-    const authToken = process.env.TWILIO_AUTH_TOKEN;
+/**
+ * createUser service creates a newUser & returns it.
+ *
+ * @param {UserDto} user is the User object to be created in the db
+ * @returns {Promise<UserSchemaDto>} is the created User document.
+ */
+const createUser = async (user: UserDto): Promise<UserSchemaDto> => {
+  const newUser = await User.create(user);
 
-    const client = require("twilio")(accountSid, authToken);
-
-    client.verify.v2
-      .services(accountSid)
-      .verifications.create({ to: user.phone, channel: "sms" })
-      .then((verification: any) => console.log(verification.sid));
-
-    var randomSlug = uniqueSlug();
-    // console.log(randomSlug);
-
-    let email = user.name.split(" ").join(".") + randomSlug + "@mindwebs.com";
-
-    const fetchUserEmail = await User.findOne({ email: email });
-    if (fetchUserEmail) {
-      randomSlug = uniqueSlug("mindwebs@ems@3.0");
-      email = user.name.split(" ").join(".") + randomSlug + "@mindwebs.com";
-    }
-
-    email = email.toLowerCase();
-
-    const salt = await bcrypt.genSalt(10);
-    const secPass = await bcrypt.hash(user.password, salt);
-
-    const newUser = await User.create({
-      name: user.name,
-      email: email,
-      password: secPass,
-      dob: user.dob,
-      age: user.age,
-      sex: user.sex,
-      phone: user.phone,
-      country: user.country,
-    });
-
-    return { code: 201, result: newUser, message: "OK" };
-  } catch (err: any) {
-    if (String(process.env.DEBUG) === "TRUE") console.log(err);
-    if (err.name === "ValidationError")
-      return { code: 400, message: "Wrong Input Format" };
-    return { code: 500, message: "Internal Server Error" };
-  }
+  return newUser;
 };
 
 export { createUser };
